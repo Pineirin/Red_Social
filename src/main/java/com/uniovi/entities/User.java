52996136b0ece139c64df933912a613f900cbedc
DESKTOP-V9D9FB7\Pineirin <pineirin@gmail.com>
2018-03-19 00:27:37 +0100

    Revert "Apartado 9 y 10 en camino"
    This reverts commit 91c068977d759edfb4833ca2c548bd38d51a9fbe.

--- src/main/java/com/uniovi/entities/User.java
+++ src/main/java/com/uniovi/entities/User.java
@@ -32,9 +32,6 @@ public class User {
 	
 	@OneToMany(mappedBy = "userOrigen", cascade = CascadeType.ALL)
 	private Set<Petition> petitions;
-	
-	@OneToMany(mappedBy="user",cascade =CascadeType.ALL)
-	private Set<Publication> publications;
 
 	public User() {
 		
@@ -131,14 +128,6 @@ public class User {
 
 	public void setEnLinea(boolean enLinea) {
 		this.enLinea = enLinea;
-	}
-
-	public Set<Publication> getPublications() {
-		return publications;
-	}
-
-	public void setPublications(Set<Publication> publications) {
-		this.publications = publications;
 	}
 
 	
